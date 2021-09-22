b43189edeeb2187461b6463dde55ae7b2949fb64
Natalia Kazakova <17986517+natakazakova@users.noreply.github.com>
2021-09-22 16:06:36 +0300

    Update Global.asax.cs

--- CS/DashboardJSControl/Global.asax.cs
+++ CS/DashboardJSControl/Global.asax.cs
@@ -1,14 +1,11 @@
-﻿using DevExpress.DashboardWeb;
+using DevExpress.DashboardWeb;
 using DevExpress.DashboardWeb.Mvc;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-
 namespace DashboardJSControl {
-    public class MvcApplication : System.Web.HttpApplication
-    {
-        protected void Application_Start()
-        {
+    public class MvcApplication : System.Web.HttpApplication {
+        protected void Application_Start() {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             RouteTable.Routes.MapDashboardRoute("api/dashboard");
